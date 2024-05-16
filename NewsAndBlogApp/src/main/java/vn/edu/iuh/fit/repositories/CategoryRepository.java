6ab654d6885e12d9048273245d58fde51ebbb38e
KidieZyllex <78770966+kidiezyllex@users.noreply.github.com>
2024-05-16 12:16:03 +0700

    Delete NewsAndBlogApp directory

--- NewsAndBlogApp/src/main/java/vn/edu/iuh/fit/repositories/CategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package vn.edu.iuh.fit.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import vn.edu.iuh.fit.models.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
